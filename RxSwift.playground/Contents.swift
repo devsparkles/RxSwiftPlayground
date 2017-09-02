06e53eb61749263c8ba6986f4b5e28b26753e4ae
Xavier Maximin <xavier.maximin.uk@gmail.com>
2017-09-02 16:39:17 +0100

    Added toObservable

--- RxSwift.playground/Contents.swift
+++ RxSwift.playground/Contents.swift
@@ -12,9 +12,29 @@ exampleOf(description: "just"){
 }
 
 
-exampleOf(description: "just"){
-    let observable = Observable.just("Hello, World")
-    observable.subscribe { (event: Event<String>) in
-        print(event)
+exampleOf(description: "of"){
+     let observable = Observable.of(1, 2, 3)
+    
+    observable.subscribe {
+        print($0)
     }
+    observable.subscribe {
+        print($0)
+    }
+}
+
+
+exampleOf(description: "toObservable"){
+    let disposeBag = DisposeBag()
+    let subscription: Disposable
+    var array: Variable<[Int]> = Variable([1, 2, 3])
+    array.asObservable()
+        .subscribe(onNext: {
+            print($0)
+            }
+            
+    )
+    .addDisposableTo(disposeBag)
+    
+    
 }
